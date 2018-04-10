f93c174d8357fd226f0393239f69b5e747408101
Eugeny Lutsenko <st.lui@live.ru>
2018-04-10 16:28:36 +0700

    Updater реализован в отдельном потоке

---
+++ TaskManager/Updater.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TaskManager
+{
+	class Updater
+	{
+		static Thread updaterThread;
+		static bool isRunning;
+
+		public static void Start()
+		{
+			ThreadStart threadStart = new ThreadStart(UpdateRunner);
+			isRunning = true;
+			updaterThread = new Thread(threadStart);
+			updaterThread.Start();
+		}
+
+		public static void Abort()
+		{
+			updaterThread.Abort();
+		}
+
+		public static void Resume()
+		{
+			isRunning = true;
+		}
+
+		public static void Suspend()
+		{
+			isRunning = false;
+		}
+		public static void Update()
+		{
+			ProcessList pl = ProcessList.TryGetInstance();
+			if (pl != null)
+			{
+				pl.List.Clear();
+				foreach (var p in Process.GetProcesses().ToList())
+				{
+					pl.List.Add(p);
+				}
+				pl.List.Sort(delegate (Process a, Process b) { return a.ProcessName.CompareTo(b.ProcessName); });
+			}
+			ProcessList.Unlock();
+		}
+
+		public static void UpdateRunner()
+		{
+			while (true)
+			{
+				if (isRunning)
+					Update();
+				Thread.Sleep(666);
+			}
+		}
+	}
+}
